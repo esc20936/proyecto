48ec056b7dcb9e44ca8e861da787be9f2fe5c0b5
esc20936 <esc20936@uvg.edu.gt>
2022-08-08 12:05:50 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import itertools
+
+def brute_force(cnf):
+    literals = set()
+    for conj in cnf:
+        for disj in conj:
+            literals.add(disj[0])
+ 
+    literals = list(literals)
+    print(literals)
+    n = len(literals)
+    for seq in itertools.product([True,False], repeat=n):
+        a = set(zip(literals, seq))
+        if all([bool(disj.intersection(a)) for disj in cnf]):
+            return True, a
+ 
+    return False, None
+
+def __select_literal(cnf):
+    for c in cnf:
+        for literal in c:
+            return literal[0]
+ 
+def dpll(cnf, assignments={}):
+ 
+    if len(cnf) == 0:
+        return True, assignments
+ 
+    if any([len(c)==0 for c in cnf]):
+        return False, None
+ 
+    l = __select_literal(cnf)
+ 
+    new_cnf = [c for c in cnf if (l, True) not in c]
+    new_cnf = [c.difference({(l, False)}) for c in new_cnf]
+    sat, vals = dpll(new_cnf, {**assignments, **{l: True}})
+    if sat:
+        return sat, vals
+ 
+    new_cnf = [c for c in cnf if (l, False) not in c]
+    new_cnf = [c.difference({(l, True)}) for c in new_cnf]
+    sat, vals = dpll(new_cnf, {**assignments, **{l: False}})
+    if sat:
+        return sat, vals
+ 
+    return False, None
+
+
+formula = [{("p", True), ("q", False)}, {("p", True), ("r", True)}]
+
+print(dpll(formula))
